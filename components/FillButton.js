3b58a4de880772f983a70ff4fdf47438b9997626
Oviedo F <oviedofederico039@gmail.com>
2022-08-07 21:16:33 -0300

    api cv

--- components/FillButton.js
+++ components/FillButton.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import styles from './FillButton.module.scss';
 import { useTranslation } from 'react-i18next';
+import axios from 'axios';
+import { saveAs } from 'file-saver';
 
 export default function FillButton() {
   const [t] = useTranslation("global");
 
+  const saveFile = () => {
+    saveAs(
+      `${process.env.HOSTNAME}cv`,
+      "federico-ov-cv.pdf"
+    );
+  };
+
   return (
-    <button type="button" className={styles.fill}>{t("About.download-cv")}</button>
+    <button onClick={(e) => saveFile()} download className={styles.fill}>{t("About.download-cv")}</button>
   )
 }
